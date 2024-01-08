6e006a4d517b1b0044f13db9ffe3f554244a85ac
TrieuVD <trieuvd@runsystem.net>
2024-01-08 22:31:18 +0700

    build base project

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,21 @@
+<template>
+  <router-view />
+  <Toast />
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BaseLayout",
+  props: {
+    namePage: {
+      type: String,
+      default: undefined,
+    },
+  },
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,15 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
-createApp(App).use(store).use(router).mount("#app");
+import ToastService from "primevue/toastservice";
+
+const app = createApp(App);
+app.use(store);
+app.use(router);
+app.use(ToastService);
+app.mount("#app");
