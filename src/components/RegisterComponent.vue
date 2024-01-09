e546ba46acca79e68764750fb84b86048a9112c2
trieuvd <trieuvd@runsystem.net>
2024-01-09 17:41:20 +0700

    Register component

--- src/components/RegisterComponent.vue
+++ src/components/RegisterComponent.vue
@@ -1,25 +1,125 @@
 <template>
-  <div class="card flex justify-content-center">
-    <InputText type="text" v-model="value" />
+  <div class="flex flex-column align-items-center">
+    <form
+      class="form flex flex-column justify-content-center"
+      action=""
+      method="get"
+    >
+      <h2 class="header-register align-self-center">Đăng kí</h2>
+      <span class="mt-4 p-float-label">
+        <InputText class="w-full" id="email" v-model="email" />
+        <label for="email">Email</label>
+      </span>
+      <span class="mt-4 p-float-label">
+        <InputText class="w-full" id="phone" v-model="phoneNumber" />
+        <label for="phone">Số điện thoại</label>
+      </span>
+      <span class="mt-4 p-float-label">
+        <InputText class="w-full" id="password" v-model="password" />
+        <label for="password">Mật khẩu</label>
+      </span>
+      <span class="mt-4 p-float-label">
+        <InputText
+          class="w-full"
+          id="passwordComfirm"
+          v-model="passwordComfirm"
+        />
+        <label for="passwordComfirm">Xác thực mật khẩu</label>
+      </span>
+
+      <div class="flex align-items-center mt-8">
+        <Checkbox
+          id="checkbox-policy"
+          v-model="checked"
+          :binary="true"
+        ></Checkbox>
+        <label for="checkbox-policy">Đồng ý với điều khoản của Shop</label>
+      </div>
+      <div class="mt-8">
+        <Button label="Đăng ký" @click.prevent="validate()"></Button>
+      </div>
+    </form>
   </div>
 </template>
 
 <script lang="ts">
+import { Validator } from "@/utils/Validator";
 import { defineComponent, ref } from "vue";
 import InputText from "primevue/inputtext";
+import Button from "primevue/button";
+import Checkbox from "primevue/checkbox";
+import { useToast } from "primevue/usetoast";
+
+interface ErrorMessage {
+  email: string;
+  phoneNumber: string;
+  password: string;
+  passwordComfirm: string;
+}
+
 export default defineComponent({
   name: "RegisterComponent",
   components: {
     InputText,
+    Button,
+    Checkbox,
   },
   setup() {
-    const value = ref("value");
+    const email = ref<string>("");
+    const phoneNumber = ref<string>("");
+    const password = ref<string>("");
+    const passwordComfirm = ref<string>("");
+    const checked = ref(false);
+
+    const toast = useToast();
+
+    let errorMessage = ref<ErrorMessage>({
+      email: "",
+      phoneNumber: "",
+      password: "",
+      passwordComfirm: "",
+    } as ErrorMessage);
+
+    const validate = () => {
+      if (Validator.isNullOrEmpty(email.value)) {
+        errorMessage.value.email = "vui lòng nhập email";
+      }
+      if (Validator.isNullOrEmpty(phoneNumber.value)) {
+        errorMessage.value.phoneNumber = "email";
+      }
+      if (Validator.isNullOrEmpty(email.value)) {
+        errorMessage.value.email = "email";
+      }
+      if (Validator.isNullOrEmpty(email.value)) {
+        errorMessage.value.email = "email";
+      }
+
+      toast.add({
+        severity: "error",
+        summary: "error",
+        detail: "mess",
+        life: 3000,
+      });
+    };
 
     return {
-      value,
+      email,
+      phoneNumber,
+      password,
+      passwordComfirm,
+      checked,
+      validate,
     };
   },
 });
 </script>
 
-<style scoped></style>
+<style scoped>
+.header-register {
+  color: rgb(63, 63, 255);
+}
+.form {
+  width: 300px;
+  height: 80vh;
+}
+</style>
--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -5,9 +5,12 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-
+import Toast from "primevue/toast";
 export default defineComponent({
   name: "BaseLayout",
+  components: {
+    Toast,
+  },
   props: {
     namePage: {
       type: String,
