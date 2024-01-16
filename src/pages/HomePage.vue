2d35818da4d71e8b65ae5b04c0621a28f42e5cf6
TrieuVD <trieuvd@runsystem.net>
2024-01-16 20:39:23 +0700

    create HomePage

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <VDTHeader />
+    <h1>Home Page</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import VDTHeader from "@/components/VDTHeader.vue";
+
+export default defineComponent({
+  name: "HomePage",
+  components: { VDTHeader },
+  setup() {
+    return {};
+  },
+});
+</script>
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,12 +2,18 @@ import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import BaseLayout from "@/layouts/BaseLayout.vue";
 import RegisterPage from "@/pages/RegisterPage.vue";
 import LoginPage from "@/pages/LoginPage.vue";
+import HomePage from "@/pages/HomePage.vue";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     component: BaseLayout,
     props: { namePage: "Base layout" },
     children: [
+      {
+        path: "/",
+        component: HomePage,
+        children: [],
+      },
       {
         path: "/register",
         component: RegisterPage,
